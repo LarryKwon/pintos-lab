373c730e0e7861f06bad7d666db783c6398146ea
larrykwon <koallarry11@kaist.ac.kr>
2022-04-21 06:38:40 +0000

    thread_unblock 구현 list_push_back 대신 list_insert_ordered로 교체

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -245,7 +245,9 @@ void thread_unblock(struct thread *t)
 
   old_level = intr_disable();
   ASSERT(t->status == THREAD_BLOCKED);
-  list_push_back(&ready_list, &t->elem);
+  
+  list_insert_ordered(&ready_list,&t->elem,cmp_priority,NULL);
+  // list_push_back(&ready_list, &t->elem);
   t->status = THREAD_READY;
   intr_set_level(old_level);
 }
