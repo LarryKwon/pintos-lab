2acd959ec71e4b985000492e1845a1204e3b6d44
larrykwon <koallarry11@kaist.ac.kr>
2022-04-23 11:34:05 +0000

    thread_set_priority 함수에 refresh_priority 추가

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -420,7 +420,8 @@ void thread_foreach(thread_action_func *func, void *aux)
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void thread_set_priority(int new_priority)
 {
-  thread_current()->priority = new_priority;
+  thread_current()->init_priority = new_priority;
+  refresh_priority();
   test_max_priority();
 }
 
